e9f22f20b7569c28a6422d2b40cb37f65ce1c6ac
CaptainYu <chenyulay@qq.com>
2024-02-20 14:24:57 +0800

    一个简单的ai移动逻辑

--- player.gd
+++ player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
+
 @export var SPEED := 500.0
 @export var JUMP_VELOCITY := 100.0
 @export var is_user_input_enabled := true
+@export var move_detla:=0.0
 
 var window_size:Vector2
 func _ready():
@@ -19,8 +21,20 @@ func _physics_process(_delta: float) -> void:
 		else:
 			velocity.y = 0
 		move_and_slide()
-		#限制角色移动范围
-		position.y = clamp(position.y, 56, window_size.y-56)
 	else:
-		#TODO: AI移动
-		pass
+		#FIXME: AI移动，还不够好
+		move_detla += _delta
+		if move_detla > 1.0:
+			move_detla = 0.0
+			velocity.y = ai_move()
+			print(velocity.y)
+		move_and_slide()
+	#限制角色移动范围
+	position.y = clamp(position.y, 56, window_size.y-56)
+
+## AI移动逻辑，并返回一个移动的速度矢量出来
+func ai_move()->float:
+	# 获得一个随机方向
+	var direction := randi_range(-1,1)
+	var new_velocity = direction * SPEED
+	return new_velocity
