dcb74658a395da4ef854c14fce931fdf02fee50f
CaptainYu <chenyulay@qq.com>
2024-03-01 21:31:42 +0800

    修改了角色移动代码

--- ball.gd
+++ ball.gd
@@ -3,22 +3,23 @@ extends RigidBody2D
 #自定义游戏结束信号
 signal game_over
 
-var initVelcity:=100.0
+var initVelcity:=-500.0
 var uiSize:Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#获取界面大小
 	uiSize=get_viewport_rect().size
+	#设置球的初始位置和禁止旋转
 	position=uiSize/2
+	lock_rotation=true
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if GlobalVar.isStart:
-		custom_integrator=false
-		linear_velocity=Vector2(500,0)
+		linear_velocity=Vector2(initVelcity,0)
 		#如果超出游戏场景就结束游戏
 		if position.x<0 or position.x>uiSize.x:
 			s_game_over()
@@ -30,4 +31,3 @@ func s_game_over()->void:
 	emit_signal("game_over")
 	print("hello")
 	queue_free()
-	
--- player.gd
+++ player.gd
@@ -2,12 +2,14 @@ extends CharacterBody2D
 
 
 
-@export var SPEED := 500.0
+@export var SPEED := 5.0
 @export var JUMP_VELOCITY := 100.0
 @export var is_user_input_enabled := true
 @export var move_detla:=0.0
 
 var window_size:Vector2
+var ai_move_value:=0.0
+
 func _ready():
 	#获取窗口大小
 	window_size = get_viewport_rect().size
@@ -19,18 +21,14 @@ func _physics_process(_delta: float) -> void:
 		# 移动角色
 		var direction := Input.get_axis("ui_up", "ui_down")
 		if direction != 0:
-			velocity.y = SPEED*direction
-		else:
-			velocity.y = 0
-		move_and_slide()
+			position.y += SPEED*direction
 	else:
 		#FIXME: AI移动，还不够好
 		move_detla += _delta
-		if move_detla > 1.0:
+		if move_detla > 0.5:
 			move_detla = 0.0
-			velocity.y = ai_move()
-			print(velocity.y)
-		move_and_slide()
+			ai_move_value=ai_move()
+		position.y += ai_move_value
 	#限制角色移动范围
 	position.y = clamp(position.y, 56, window_size.y-56)
 
